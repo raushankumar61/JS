fad727a58613c5fd5bf95773b11b1f4e7ea760fe
Raushan Kumar <115101795+raushankumar61@users.noreply.github.com>
2026-04-07 15:59:59 +0000

    understood diff datatypes in js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treats all JS code as newer version
+
+console.log(3+3)
+console.log("Hello")
+let name = "Raushan"
+let age = 18
+let isLoggedIn = false
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null) //object
+
+
